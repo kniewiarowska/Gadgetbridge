d6dca152371c6e1e4d266f0438752a188970eef8
kniewiarowska <karolina-niew@wp.pl>
2023-01-06 21:25:48 +0100

    add Hsense services

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/hsense/HSenseService.java
@@ -0,0 +1,38 @@
+package nodomain.freeyourgadget.gadgetbridge.service.hsense;
+import android.content.Context;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.List;
+import java.util.Properties;
+import java.util.Timer;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import nodomain.freeyourgadget.gadgetbridge.database.HSenseDataExporter;
+import nodomain.freeyourgadget.gadgetbridge.service.hsense.HSenseClient;
+
+public class HSenseService {
+    private HSenseClient hSenseClient;
+    private String lastTimeStamp = "0";
+    private final int MINUTES = 60;
+    public Timer timer = new Timer();
+
+}
+
